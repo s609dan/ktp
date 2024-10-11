b7f1700e7d8e94c18f5d3dcf89623bb33ac3cf8b
s609dan <82160309+s609dan@users.noreply.github.com>
2024-10-11 21:50:38 +0300

    Update lab3.cpp

--- lab3.cpp
+++ lab3.cpp
@@ -1,33 +1,41 @@
 #include <iostream>
-#include <cmath>
+#include <math.h>
 using std::cout;
 using std::cin;
 using std::endl;
 
-double arccot(double x) {
-    return M_PI / 2 - atan(x);
+double arcctg(double x) {
+    return M_PI_2 - atan(x);  // M_PI_2 - це π/2
 }
 
 int main()
 {
     float t, x, y;
+
     cout << "Please enter t = ";
     cin >> t;
+
     x = t + 0.7;
     cout << "x = " << x << endl;
 
-    
     if (x < 0) {
         y = x - exp(x);
-    } 
+    }
     else if (x >= 2 && x < 5 && x != 3) {
-        y = arccot(x + sqrt(x - 1.4));
-    } 
-    else {
-        cout << "Значення x не підпадає під задані умови." << endl;
-        return 1;
+        arcctg(x + sqrt(x) - 1.4);
+    } else {
+        cout << "the value does not meet the condition" << endl;
     }
 
     cout << "Результат: y = " << y << endl;
     return 0;
 }
+
+
+
+
+// int main() {
+//     double x = 1.0;
+//     std::cout << "arcctg(" << x << ") = " << arcctg(x) << std::endl;
+//     return 0;
+// }
